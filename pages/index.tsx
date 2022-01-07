89ea0356f5bffb67bc782c11aa983503ac70a41f
Jere Suikkila <jere.suikkila@mappedin.ca>
2022-01-07 15:24:08 -0500

    Remove footer. Add 1 fact

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,16 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-import {facts} from '../public/beaverfacts.json';
+import facts from "../public/beaverfacts.json";
 
 const BeaverFact = () => {
+  const randomFact =
+    facts.facts[Math.floor(Math.random() * facts.facts.length)];
 
-  const randomFact = facts[Math.floor(Math.random() * facts.length)];
-
-  return (
-    <p>{randomFact.fact}</p>
-  )
-}
+  return <p>{randomFact.fact}</p>;
+};
 
 const Home: NextPage = () => {
   return (
@@ -24,29 +22,16 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <Image 
-          src="/Beaverhugs1.png" 
+        <Image
+          src="/Beaverhugs1.png"
           alt="Drawing of a beaver hugging a maple leaf"
           width={500}
           height={500}
         />
         <BeaverFact />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
